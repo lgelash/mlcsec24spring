04a866726cad8d69f8694833df43acd0a5ffee01
lgelash <162736678+lgelash@users.noreply.github.com>
2024-04-24 21:10:08 +0400

    Add files via upload

---
+++ data_processor2.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import ipaddress
+from sklearn.preprocessing import LabelEncoder
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("Darknet.csv")
+
+df = df.drop(["Flow ID", "Timestamp", "Label2", "Src IP", "Dst IP"], axis=1)
+
+df = df.dropna()
+
+label_encoder1 = LabelEncoder()
+df['Label1'] = label_encoder1.fit_transform(df['Label1'])
+
+df.to_csv("processed.csv", index=False)
+
+df = pd.read_csv("processed.csv")
+
+features = df.drop(['Label1'], axis=1)
+label = df['Label1']
+
+X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.2, random_state=42)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(250, activation='relu', input_shape=(features.shape[1],)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(4, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=10, batch_size=256, validation_data=(X_test, y_test))
+
+loss, accuracy = model.evaluate(X_test, y_test)
+print(f'Test Loss: {loss:.4f}')
+print(f'Test Accuracy: {accuracy:.4f}')
+
+with open('accuracy2.txt', 'w') as f:
+    f.write(f'Test Loss: {loss:.4f}\n')
+    f.write(f'Test Accuracy: {accuracy:.4f}\n')
